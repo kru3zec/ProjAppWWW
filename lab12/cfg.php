95909acbee83b3d3c284b422732eee65b55641ad
mtszkruszewski <106517097+kru3zec@users.noreply.github.com>
2025-01-15 21:44:51 +0100

    lab12

--- lab12/cfg.php
+++ lab12/cfg.php
@@ -1,26 +1,14 @@
 <?php
-session_start(); // Rozpoczęcie sesji
-/*
- Konfiguracja podstawowa dla aplikacji:
- - Dane połączenia z bazą danych
- - Stałe logowania dla administratora
- - Ustawienia globalne
- */
-$dbhost= 'localhost';
-$dbuser= 'root';
-$dbpass = '';
-$baza = 'moja_strona';
-$login = 'login';
-$pass = 'haslo';
-$email_pass= 'Kam!paw2';
+// utwórz nową sesję i zmienne środowiskowe
+session_start();
+    $dbhost= 'localhost';
+    $dbuser= 'root';
+    $dbpass = '';
+    $baza = 'moja_strona';
+    $login = 'admin';
+    $pass = 'admin';
 
-
-// Nawiązanie połączenia z bazą danych
-$link = mysqli_connect($dbhost,$dbuser,$dbpass);
-if (!$link) {
-    echo '<b>przerwane połączenie </b>'; // Komunikat o błędzie połączenia
-}
-if(!mysqli_select_db($link, $baza)) {
-    echo 'nie wybrano bazy'; // Komunikat o błędzie wyboru bazy danych
-}
+    $link = mysqli_connect($dbhost,$dbuser,$dbpass);
+    if (!$link) echo '<b>przerwane połączenie </b>';
+    if(!mysqli_select_db($link, $baza)) echo 'nie wybrano bazy';
 ?>
